a240febf8fdeb0f46d2dc84a322cd70a335c43d6
Jonas Salo <salomannen1234@gmail.com>
2026-03-14 08:59:06 +0100

    R12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,8 +184,3 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 
 # se_omapi
 TW_INCLUDE_OMAPI := true
-#
-
-BOARD_RECOVERY_IMAGE_PREPARE += \
-cp -f $(DEVICE_PATH)/recovery/root/vendor/bin/keystore2_v36 $(TARGET_RECOVERY_ROOT_OUT)/system/bin/keystore2; \
-cp -f $(DEVICE_PATH)/recovery/root/vendor/lib64/libc++_v36.so $(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libc++_v36.so;
